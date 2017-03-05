27ffa8d91e21f3385851320d0508f661fa6e04f6
A.Nesterkin <ul.morhekil@gmail.com>
2017-03-05 23:52:04 +0400

    Add build script

---
+++ build.bat
@@ -0,0 +1,6 @@
+set BOOST_INC=C:\Users\void\develop\boost_1_63_0\
+set BOOST_LIB=C:\Users\void\develop\boost_1_63_0\
+set PLATF="x64"
+set CONF=Release
+
+msbuild FXTM-client-server.sln /p:BOOST_INC_PATH=%BOOST_INC% /p:BOOST_LIB_PATH=%BOOST_LIB% /p:Configuration=%CONF% /p:Platform=%PLATF%
