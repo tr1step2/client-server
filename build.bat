46e0622abe77f8053d87d73275a64f79eb235bb2
A.Nesterkin <ul.morhekil@gmail.com>
2017-03-06 13:34:57 +0400

    Fix build script

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
+@echo off
+
 set BOOST_INC=C:\Users\void\develop\boost_1_63_0\
-set BOOST_LIB=C:\Users\void\develop\boost_1_63_0\
+set BOOST_LIB=C:\Users\void\develop\boost_1_63_0\stage64\lib
 set PLATF="x64"
 set CONF=Release
 
